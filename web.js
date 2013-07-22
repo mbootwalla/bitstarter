fded7747fc701981ecb4c3ce749cb5be4e13405a
Moiz Bootwalla <msbootwalla@gmail.com>
2013-07-22 12:06:04 +0000

    Added responsive styling via bootstrap and modified web.js to listen on port 8080

--- web.js
+++ web.js
@@ -4,11 +4,14 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
+var htmlfile = "index.html";
+
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('./index.html').toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
